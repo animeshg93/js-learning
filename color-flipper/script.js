9d256f2b3c2bc5c0a84e6bceeac3e6da18333b85
gandhia <animesh.gandhi@earlywarning.com>
2023-10-27 22:31:16 -0700

    Completed html

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,3 +1,5 @@
 function getColor() {
-    return document.body.style.backgroundColor;
+    const color = document.getElementById("color-name");
+    color.innerHTML = "#f1f5f4"
+    document.body.style.backgroundColor = "#f1f5f4"
   }
