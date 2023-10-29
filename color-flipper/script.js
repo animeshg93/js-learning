d0ec9210bf163f31b8f7cb8ac9266387b857a0de
gandhia <animesh.gandhi@earlywarning.com>
2023-10-28 21:59:42 -0700

    Added random hex

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -6,3 +6,19 @@ function changeColor() {
   bgcolor.innerHTML = randomColor
   document.body.style.backgroundColor = randomColor
 }
+
+function changeHex() {
+  var bgcolor = document.getElementById("color-name");
+  var randomColor = getRandomColor();
+  bgcolor.innerHTML = randomColor
+  document.body.style.backgroundColor = randomColor
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
