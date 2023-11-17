1dbf6c386f1ab0b064d58a3832fbf0399a763dc1
gandhia <animesh.gandhi@earlywarning.com>
2023-11-16 22:26:00 -0700

    Added random text generation

--- js-reviews/script.js
+++ js-reviews/script.js
@@ -25,5 +25,20 @@ const getUser = (isRandom, isInc) => {
     document.getElementById("pic").src = person["picture"]["large"]
     document.getElementById("location").innerHTML = person["location"]["country"]
 
+    generateParagraph()
+}
 
+const generateParagraph = () => {
+    const wordList = ["auditor", "fixture", "eye", "cheque", "van", "cereal", "burst", "outline", "volcano", "sport", "faith",
+        "exit", "murder", "justify", "funeral", "opera", "hold", "aunt", "excess", "galaxy", "smile", "exhibition", "multiply",
+        "continuous", "laboratory", "pray", "virtue", "freight", "kick", "technique", "brake", "demonstrator", "red", "projection",
+        "swell", "will", "crouch", "specimen", "welfare", "spare", "original", "discovery", "goalkeeper", "syndrome", "inject", "can",
+        "paralyzed", "elect", "victory", "evaluate"]
+
+    var text = '';
+    for (var i = 0; i < wordList.length; i++) {
+        text += wordList[Math.floor(Math.random() * wordList.length)] + " ";
+    }
+
+    document.getElementById("text").innerHTML = text
 }
